572acbf90f60a017e6ebc00643f3b1e2625f1b25
strahsvid <maximsushkin02@gmail.com>
2020-10-29 10:52:06 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        PasswordGenerator passwordGenerator = new PasswordGenerator.PasswordGeneratorBuilder().
+            useDigits(false).
+            useLower(false).
+            useUpper(false).
+            usePunctuation(true).
+            build();
+
+        String password = passwordGenerator.generate(5);
+
+        System.out.println("\n\n" + password + "\n");
+    }    
+}
